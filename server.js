a34d45932a39ed674223768175c1e1b22bdc51a7
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-17 02:20:27 -0300

    socket io connection and disconnection detection

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const server = http.createServer(app)
 const socketio = require('socket.io')
 const io = socketio(server)
 
-const PORT = 8080 || process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('./public'))
 
 io.on('connection', (socket) => {
     console.log('New connection!')
+
+    socket.emit('message', 'Hello!')
+
+    socket.broadcast.emit('message' ,'Someone has joined!')
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'Disconnection detected!')
+    })
 })
 
 server.listen(PORT, () => console.log(`Live on port ${PORT}`))
