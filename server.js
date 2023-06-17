34fe08a0dd30b6949653d348fce9b7f904ef4f38
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-17 02:04:29 -0300

    socket.io setup

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
+const http = require('http')
+const server = http.createServer(app)
+
+const socketio = require('socket.io')
+const io = socketio(server)
+
 const PORT = 8080 || process.env.PORT
 
 app.use(express.static('./public'))
 
-app.listen(PORT, () => console.log(`Live on port ${PORT}`))
+io.on('connection', (socket) => {
+    console.log('New connection!')
+})
+
+server.listen(PORT, () => console.log(`Live on port ${PORT}`))
