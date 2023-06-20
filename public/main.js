c7e76a9be1f8404e01f5180730b5a7912c44fe95
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-19 23:11:39 -0300

    changes to html and css

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+let currentPlay = 1
+
 const socket = io()
 socket.emit('joinRoom', getRoomName())
 
@@ -11,4 +13,18 @@ function joinRoom(roomName){
 
 function getRoomName(){
     return new URLSearchParams(window.location.search).get('room') || undefined
+}
+
+
+//-------------------------------------//
+// ONLY GAME LOGIC UNDER THIS COMMENT //
+//-----------------------------------//
+
+//i should probably optmize this later
+function pushNumberToPlayStack(number){
+    currentPlay = number
+}
+
+function getLatestPlay(){
+    return currentPlay
 }
